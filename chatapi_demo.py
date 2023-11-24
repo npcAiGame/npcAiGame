3e044b31267f27103273a2ac1d10289041bbcb2c
liuyingduo <A2711985388@163.com>
2023-11-24 16:47:56 +0800

    上传python接口调用

---
+++ chatapi_demo.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+API_KEY = "Cv5wqNhPnO90oQHnmTGt42Sr"
+SECRET_KEY = "o1oYzs6bSxwTBI8Vl3bVlcmLlnobS0eA"
+
+def main():
+        
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token=" + get_access_token()
+    
+    payload = json.dumps({
+        "messages": [
+            {
+                "role": "user",
+                "content": "你好"
+            }
+        ]
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    content = response.json()
+    result = content.get("result")
+    print(result)
+    return result
+    
+
+def get_access_token():
+    """
+    使用 AK，SK 生成鉴权签名（Access Token）
+    :return: access_token，或是None(如果错误)
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
+    return str(requests.post(url, params=params).json().get("access_token"))
+
+if __name__ == '__main__':
+    main()
